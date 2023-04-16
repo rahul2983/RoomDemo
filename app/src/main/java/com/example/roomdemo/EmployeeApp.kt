f1e4a415c6f7d64b75c13d060f4a67bb8ead3771
Rahul Verma <rahul.uoa@gmail.com>
2023-04-15 19:05:41 -0700

    Initial commit

---
+++ app/src/main/java/com/example/roomdemo/EmployeeApp.kt
@@ -0,0 +1,10 @@
+package com.example.roomdemo
+
+import android.app.Application
+
+class EmployeeApp: Application() {
+
+    val db by lazy {
+        EmployeeDatabase.getInstance(this)
+    }
+}
---
+++ app/src/main/java/com/example/roomdemo/EmployeeDao.kt
@@ -0,0 +1,23 @@
+package com.example.roomdemo
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface EmployeeDao {
+
+    @Insert
+    suspend fun insert(employeeEntity: EmployeeEntity)
+
+    @Update
+    suspend fun update(employeeEntity: EmployeeEntity)
+
+    @Delete
+    suspend fun delete(employeeEntity: EmployeeEntity)
+
+    @Query("SELECT * FROM `employee-table`")
+    fun fetchAllEmployees(): Flow<List<EmployeeEntity>>
+
+    @Query("SELECT * FROM `employee-table` where id=:id")
+    fun fetchAllEmployeeById(id: Int): Flow<EmployeeEntity>
+}
