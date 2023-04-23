fa8744fd12d92edc1d96fef99ebc19ec924a08c4
Rahul Verma <rahul.uoa@gmail.com>
2023-04-23 12:58:54 -0700

    Delete and Update Items features

--- app/src/main/java/com/example/roomdemo/ItemAdapter.kt
+++ app/src/main/java/com/example/roomdemo/ItemAdapter.kt
@@ -7,9 +7,9 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.roomdemo.databinding.ItemsRowBinding
 
 class ItemAdapter(
-    private val items: ArrayList<EmployeeEntity>
-//    private val updateListener:(id:Int)->Unit,
-//    private val deleteListener:(id:Int)->Unit
+    private val items: ArrayList<EmployeeEntity>,
+    private val updateListener:(id:Int)->Unit,
+    private val deleteListener:(id:Int)->Unit
 ): RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
     class ViewHolder(binding: ItemsRowBinding): RecyclerView.ViewHolder(binding.root) {
@@ -18,7 +18,6 @@ class ItemAdapter(
         val tvEmail = binding.tvEmail
         val ivDelete = binding.ivDelete
         val ivEdit = binding.ivEdit
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -49,12 +48,11 @@ class ItemAdapter(
         }
 
         holder.ivEdit.setOnClickListener() {
-//            updateListener.invoke(item.id)
+            updateListener.invoke(item.id)
         }
 
         holder.ivDelete.setOnClickListener() {
-//            deleteListener.invoke(item.id)
+            deleteListener.invoke(item.id)
         }
-
     }
 }
--- app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.roomdemo
 
+import android.app.AlertDialog
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,6 +9,7 @@ import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomdemo.databinding.ActivityMainBinding
+import com.example.roomdemo.databinding.DialogUpdateBinding
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -51,7 +54,17 @@ class MainActivity : AppCompatActivity() {
         employeeDao: EmployeeDao
     ) {
         if (employeesList.isNotEmpty()) {
-            val itemAdapter = ItemAdapter(employeesList)
+            val itemAdapter = ItemAdapter(
+                employeesList,
+                {
+                    updateId ->
+                    updateRecordDialog(updateId, employeeDao)
+                },
+                {
+                    deleteId ->
+                    deleteRecordAlertDialog(deleteId, employeeDao)
+                }
+            )
             binding?.rvItemsList?.layoutManager = LinearLayoutManager(this)
             binding?.rvItemsList?.adapter = itemAdapter
             binding?.rvItemsList?.visibility = View.VISIBLE
@@ -60,5 +73,87 @@ class MainActivity : AppCompatActivity() {
             binding?.rvItemsList?.visibility = View.GONE
             binding?.tvNoRecordsAvailable?.visibility = View.VISIBLE
         }
+    }
+
+    private fun updateRecordDialog(id: Int, employeeDao: EmployeeDao) {
+        val updateDialog = Dialog(
+            this,
+            R.style.Theme_Dialog
+        )
+        updateDialog.setCancelable(false)
+        val binding = DialogUpdateBinding.inflate(layoutInflater)
+        updateDialog.setContentView(binding.root)
+
+        lifecycleScope.launch {
+            employeeDao.fetchAllEmployeeById(id).collect{
+                if (it != null) {
+                    binding.etUpdateName.setText(it.name)
+                    binding.etUpdateEmailId.setText(it.email)
+                }
+            }
+        }
+
+        updateDialog.show()
+
+        binding.tvUpdate.setOnClickListener {
+            val name = binding.etUpdateName.text.toString()
+            val email = binding.etUpdateEmailId.text.toString()
+
+            if (name.isNotEmpty() && email.isNotEmpty()) {
+                lifecycleScope.launch {
+                    employeeDao.update(EmployeeEntity(id, name, email))
+                    Toast.makeText(
+                        applicationContext,
+                        "Record updated",
+                        Toast.LENGTH_LONG).show()
+                    updateDialog.dismiss()
+                }
+            } else {
+                Toast.makeText(
+                    applicationContext,
+                    "Name or Email cannot be blank",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            binding.tvCancel.setOnClickListener {
+                updateDialog.dismiss()
+            }
+
+
+        }
+
+    }
+
+    private fun deleteRecordAlertDialog(id:Int, employeeDao: EmployeeDao) {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Delete Record")
+        lifecycleScope.launch {
+            employeeDao.fetchAllEmployeeById(id).collect {
+                if (it != null) {
+                    builder.setMessage("Are you sure you want to delete ${it.name}?")
+                    builder.setIcon(android.R.drawable.ic_dialog_alert)
+                }
+            }
+        }
+
+        builder.setPositiveButton("Yes"){ dialogInterface, _ ->
+            lifecycleScope.launch {
+                employeeDao.delete(EmployeeEntity(id))
+                Toast.makeText(
+                    applicationContext,
+                    "Record deleted successfully",
+                    Toast.LENGTH_SHORT).show()
+            }
+            dialogInterface.dismiss()
+
+        }
+
+        builder.setNegativeButton("No") { dialogInterface, _ ->
+            dialogInterface.dismiss()
+        }
+
+        val alertDialog: AlertDialog = builder.create()
+        alertDialog.setCancelable(false)
+        alertDialog.show() // Add this line to show the dialog
     }
 }
